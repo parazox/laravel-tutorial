c0a5d72025025b4ccb9aa38c09bba52f075c1d36
nakaotetsu <parazox@gmail.com>
2018-05-02 14:53:44 +0900

    tutorial

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
+Route::get('/', 'ArticleController@index');
+Route::get('create', 'ArticleController@create');
+Route::post('create', 'ArticleController@store');
+Route::get('edit/{id}', 'ArticleController@edit');
+Route::post('edit', 'ArticleController@update');
+Route::get('delete/{id}', 'ArticleController@show');
+Route::post('delete', 'ArticleController@delete');
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+ */
